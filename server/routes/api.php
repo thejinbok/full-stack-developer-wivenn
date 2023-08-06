5b3840f07943774752da5ff4eb50415a69d463c7
Jin Bok <jin@quatredeux.com>
2023-08-06 13:05:11 -0300

    docs(server): document department BREAD API endpoints
    Related to #1.

--- server/routes/api.php
+++ server/routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::view('/stoplight', 'stoplight')->name('stoplight');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
