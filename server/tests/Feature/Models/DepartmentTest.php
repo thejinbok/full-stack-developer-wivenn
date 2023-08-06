c985b806419d3e84604d06beccbf4f2669972b4d
Jin Bok <jin@quatredeux.com>
2023-08-06 10:39:36 -0300

    test(server): test if departments can be soft deleted
    Related to #1.

--- server/tests/Feature/Models/DepartmentTest.php
+++ server/tests/Feature/Models/DepartmentTest.php
@@ -11,16 +11,30 @@ class DepartmentTest extends TestCase
 {
     use RefreshDatabase;
 
+    private $department;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->department = Department::factory()->create();
+    }
+
     public function test_departments_can_be_instantiated(): void
     {
-        $department = Department::factory()->create();
+        $this->assertInstanceOf(Department::class, $this->department);
 
-        $this->assertInstanceOf(Department::class, $department);
+        $this->assertModelExists($this->department);
 
-        $this->assertModelExists($department);
+        $this->assertNotEmpty($this->department->name);
+        $this->assertIsString($this->department->name);
+        $this->assertTrue(50 <= Str::length($this->department->name));
+    }
 
-        $this->assertNotEmpty($department->name);
-        $this->assertIsString($department->name);
-        $this->assertTrue(50 <= Str::length($department->name));
+    public function test_departments_can_be_soft_deleted(): void
+    {
+        $this->department->delete();
+
+        $this->assertModelExists($this->department);
     }
 }
